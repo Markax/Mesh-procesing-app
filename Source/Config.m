9e69c0961aae56cb328e6280fe77f5ff87056a9e
Markax <marka.phirae@gmail.com>
2018-07-10 22:50:10 +0200

    More code implemented
    -Implemented added code to 'Generate SH' function.
    -Added new options to config menu

--- Source/Config.m
+++ Source/Config.m
@@ -22,7 +22,7 @@ function varargout = Config(varargin)
 
 % Edit the above text to modify the response to help Config
 
-% Last Modified by GUIDE v2.5 07-Jul-2018 21:21:52
+% Last Modified by GUIDE v2.5 10-Jul-2018 22:08:01
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -63,8 +63,16 @@ guidata(hObject, handles);
 fid = fopen('config.txt');
 sigma = fgetl(fid);
 numiter = fgetl(fid);
-set(handles.edit1, 'String', sigma)
-set(handles.edit2, 'String', numiter)
+minLregressLocal = fgetl(fid);
+maxLregressLocal = fgetl(fid);
+minLregressGlobal = fgetl(fid);
+maxLregressGlobal = fgetl(fid);
+set(handles.edit1, 'String', sigma);
+set(handles.edit2, 'String', numiter);
+set(handles.edit3, 'String', minLregressLocal);
+set(handles.edit4, 'String', maxLregressLocal);
+set(handles.edit5, 'String', minLregressGlobal);
+set(handles.edit6, 'String', maxLregressGlobal);
 
 
 % --- Outputs from this function are returned to the command line.
@@ -85,13 +93,25 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 sigma = str2double(get(handles.edit1, 'String'));
 numiter = str2double(get(handles.edit2, 'String'));
-if (isnan(sigma) || isnan(numiter))
-    errordlg('Values must be a number');
+minLLocal = str2double(get(handles.edit3, 'String'));
+maxLLocal = str2double(get(handles.edit4, 'String'));
+minLGlobal = str2double(get(handles.edit5, 'String'));
+maxLGlobal = str2double(get(handles.edit6, 'String'));
+if (isnan(sigma) || isnan(numiter) || isnan(minLLocal) ||isnan(maxLLocal) || isnan(minLGlobal) || isnan(maxLGlobal))
+    errordlg('Values must be a number', 'Value error');
 else
-    fid = fopen('config.txt', 'w');
-    fprintf(fid, '%d \n', sigma);
-    fprintf(fid, '%d', numiter);
-    close
+    if ( minLLocal > maxLLocal || minLGlobal > maxLGlobal )
+        errordlg('"max L regression" value must be greater than "min L regression"', 'Value error');
+    else
+        fid = fopen('config.txt', 'w');
+        fprintf(fid, '%d \n', sigma);
+        fprintf(fid, '%d \n', numiter);
+        fprintf(fid, '%d \n', minLLocal);
+        fprintf(fid, '%d \n', maxLLocal);
+        fprintf(fid, '%d \n', minLGlobal);
+        fprintf(fid, '%d', maxLGlobal);
+        close
+    end
 end
 
 
@@ -147,3 +167,95 @@ function pushbutton3_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 close
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit3 as text
+%        str2double(get(hObject,'String')) returns contents of edit3 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit3_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit4_Callback(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit4 as text
+%        str2double(get(hObject,'String')) returns contents of edit4 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit5_Callback(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit5 as text
+%        str2double(get(hObject,'String')) returns contents of edit5 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit5_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit6_Callback(hObject, eventdata, handles)
+% hObject    handle to edit6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit6 as text
+%        str2double(get(hObject,'String')) returns contents of edit6 as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function edit6_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
