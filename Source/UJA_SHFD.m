e5e2b8caedfd24a8f980b082347b83f04a6dd232
Markax <marka.phirae@gmail.com>
2018-08-04 20:18:30 +0200

    Name change
    GUI -> UJA_SHFD

--- Source/UJA_SHFD.m
+++ Source/UJA_SHFD.m
@@ -1,26 +1,26 @@
-function varargout = GUI(varargin)
-% GUI MATLAB code for GUI.fig
-%      GUI, by itself, creates a new GUI or raises the existing
+function varargout = UJA_SHFD(varargin)
+% UJA_SHFD MATLAB code for UJA_SHFD.fig
+%      UJA_SHFD, by itself, creates a new GUI or raises the existing
 %      singleton*.
 %
-%      H = GUI returns the handle to a new GUI or the handle to
+%      H = UJA_SHFD returns the handle to a new UJA_SHFD or the handle to
 %      the existing singleton*.
 %
-%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      UJA_SHFD('CALLBACK',hObject,eventData,handles,...) calls the local
 %      function named CALLBACK in GUI.M with the given input arguments.
 %
-%      GUI('Property','Value',...) creates a new GUI or raises the
+%      UJA_SHFD('Property','Value',...) creates a new UJA_SHFD or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before GUI_OpeningFcn gets called.  An
+%      applied to the GUI before UJA_SHFD_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to GUI_OpeningFcn via varargin.
+%      stop.  All inputs are passed to UJA_SHFD_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help GUI
+% Edit the above text to modify the response to help UJA_SHFD
 
 % Last Modified by GUIDE v2.5 03-Aug-2018 20:25:28
 
@@ -28,8 +28,8 @@ function varargout = GUI(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @GUI_OpeningFcn, ...
-                   'gui_OutputFcn',  @GUI_OutputFcn, ...
+                   'gui_OpeningFcn', @UJA_SHFD_OpeningFcn, ...
+                   'gui_OutputFcn',  @UJA_SHFD_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -43,15 +43,15 @@ else
 end
 % End initialization code - DO NOT EDIT
 
-% --- Executes just before GUI is made visible.
-function GUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before UJA_SHFD is made visible.
+function UJA_SHFD_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to GUI (see VARARGIN)
+% varargin   command line arguments to UJA_SHFD (see VARARGIN)
 
-% Choose default command line output for GUI
+% Choose default command line output for UJA_SHFD
 handles.output = hObject;
 
 % Update handles structure
@@ -72,12 +72,12 @@ fprintf(fset, '1 \n');
 fprintf(fset, '900 \n');
 
 
-% UIWAIT makes GUI wait for user response (see UIRESUME)
+% UIWAIT makes UJA_SHFD wait for user response (see UIRESUME)
 % uiwait(handles.MainGUI);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
+function varargout = UJA_SHFD_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
